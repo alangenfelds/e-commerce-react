44b0a4ad36faea0e6435c4639e6513c0ced2f179
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-03-18 18:05:32 +0200

    created Redux reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-import Home from "./pages/home";
+import Home from "./pages/home/Home";
+import ItemDetails from "./pages/itemDetails/ItemDetails";
+import Checkout from "./pages/checkout/Checkout";
+import Confirmation from "./pages/checkout/Confirmation";
+import Navbar from "./components/Navbar";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -17,9 +21,13 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" exact element={<Checkout />} />
+          <Route path="checkout/success" exact element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-import "./index.css";
+import cartReducer from "./state";
 import { theme } from "./theme";
 import App from "./App";
+import "./index.css";
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
