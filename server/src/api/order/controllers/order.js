61f038d88efff2bdf1586ccfd5289c442bf6f5fc
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2023-03-21 12:15:35 +0200

    updated Strapi with Stripe payment

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,9 +1,63 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, userName, email } = ctx.request.body;
+
+    try {
+      // retrieve item information from the database
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+
+      //create a stripe checkout session
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
+
+      //create an order in the database
+      await strapi.service("api::order.order").create({
+        data: {
+          userName,
+          products,
+          email,
+          stripeSessionId: session.id,
+        },
+      });
+
+      //send the session id to the client
+      return { id: session.id };
+    } catch (error) {
+      console.log(error);
+      ctx.response.status = 500;
+      return { error: { message: "Failed to create an order payment" } };
+    }
+  },
+}));
